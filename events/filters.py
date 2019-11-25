5365dfae16cf4201eb7b843a05808546f6b55c45
Ubuntu <ubuntu@ip-172-31-18-186.ec2.internal>
2019-11-25 03:39:18 +0000

    changed filter to description contains instead of name contains

--- events/filters.py
+++ events/filters.py
@@ -2,8 +2,8 @@ from .models import Event
 import django_filters
 
 class EventFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    description = django_filters.CharFilter(lookup_expr='icontains')
     
     class Meta:
         model = Event
-        fields = ['name', 'location', 'tags' ]
+        fields = ['description', 'location', 'tags' ]
