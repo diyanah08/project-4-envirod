dff2d52346b14dfeb0f0669b64d5c234f7bdd6f4
Ubuntu <ubuntu@ip-172-31-18-186.ec2.internal>
2019-11-26 07:10:18 +0000

    added requirements.txt for heroku deployment

--- EnviroD/settings.py
+++ EnviroD/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,12 +80,14 @@ WSGI_APPLICATION = 'EnviroD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse('postgres://iuijewwtzawklf:965d0f6614cf438718a3b8b341e4149eaab4d9c60c17f4c6691ec8c5fd294efa@ec2-174-129-254-235.compute-1.amazonaws.com:5432/dedlliih324b2k')}
 
 
 # Password validation
